dd15c6966cad89551776015ade5437678587d838
Mohamed Akeel <akeel1447@gmail.com>
2021-08-13 15:18:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 import Login from "./components/Login";
+import CreateClassTimetable from "./components/classTimetableManagement/CreateClassTimetable";
+import CreateExamTimetable from "./components/examTimetableManagement/CreateExamTimetable";
 
 function App() {
   return <div>
     <Router>
       <Switch>
-          <Route path="/login" component={Login}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/createClassTimetable" component={CreateClassTimetable}/>
+        <Route path="/createExamTimetable" component={CreateExamTimetable}/>
+        <Route path="/storeResults" component={CreateClassTimetable}/>
       </Switch>
     </Router>
   </div>
---
+++ src/components/userManagement/CreateUserAccount.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class CreateUserAccount extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state ={
+
+        }
+    }
+
+    render() {
+        return <div>
+
+        </div>
+    }
+}
+
+export default CreateUserAccount;
