95c6d3d8c3a32118131d8dc8f4b3a08fc5f6c2f2
Mohamed Akeel <akeel1447@gmail.com>
2021-08-13 15:23:50 +0530

    ManageUserAccounts.jsx created in user management

---
+++ src/components/userManagement/ManageUserAccounts.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class ManageUserAccounts extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state ={
+
+        }
+    }
+
+    render() {
+        return <div>
+
+        </div>
+    }
+
+
+}
+
+export default ManageUserAccounts;
