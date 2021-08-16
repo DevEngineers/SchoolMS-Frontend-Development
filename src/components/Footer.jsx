ff383bb6a72e0039fd940b49b734c41206aa01cc
Zumry <amzumry@gmail.com>
2021-08-16 13:44:43 +0530

    Footer UI Design implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import '../styles/HeaderFooter.css';
+import Button from '@material-ui/core/Button';
 
 /**
  * @author : A.M Zumry
@@ -6,16 +8,42 @@ import React, {Component} from 'react';
  */
 
 class Footer extends Component {
-    // constructor(props){
-    //     super(props);
-    // }
+    constructor(props){
+        super(props);
+
+        this.state= {
+            type:"Administrator"
+        }
+    }
+
+    componentDidMount() {
+        this.setState({type:localStorage.getItem('type')})
+    }
+
+    componentWillUnmount() {
+        localStorage.removeItem('footerValue')
+    }
 
     render() {
-        return (
-            <div>
-                
-            </div>
-        );
+        return <div>
+            {
+                localStorage.getItem('footerValue') !== 'value' ?
+                    (
+                        this.state.type === 'Administrator'?
+                            (<></>)
+                            :(<div id={'footer'}>
+                                <div id={'copyright'}>
+                                    <label id={'contInfoLabel'}>Copyright &copy;2021 Gateway International School all right reserved</label>
+                                    {/*<hr/>*/}
+                                </div>
+                                <div id="about-footer">
+                                    <Button variant="contained" color="primary"> About US</Button>
+                                </div>
+                            </div>)
+                    )
+                    :(<></>,localStorage.removeItem('footerValue'))
+            }
+        </div>
     }
 }
 
