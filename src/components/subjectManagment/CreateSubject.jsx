155a92127ffcd91dc0f8623d1bbcf4deb82db470
Zumry <amzumry@gmail.com>
2021-08-17 16:05:38 +0530

    CreateSubject UI Design implementing

---
+++ src/components/subjectManagment/CreateSubject.jsx
@@ -0,0 +1,121 @@
+import React, {Component} from 'react';
+import {Box, Grid, MenuItem} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
+
+/**
+ * @author : A.M Zumry
+ * Registration Number : IT19175126
+ */
+
+class CreateSubject extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            grade:['A','B','C'],
+            teacher:['Nimal', 'Kumar', 'Kasun']
+        }
+    }
+
+    /**
+     * this function is to capture data in the input fields
+     */
+    onChange(event){
+        const { name, value } = event.target;
+        this.setState({ [name] : value });
+    }
+
+    render() {
+        return <div className={"subject-section"}>
+            <div className={"subject-container"}>
+
+                <div className={"subject-row"}>
+                    <div className={"subject-section-title"}>
+                        <h2 className={"subject-custom-underline"}> CREATE CLASS </h2>
+                    </div>
+                </div>
+
+                <div className={"subject-row"}>
+                    <div className={"subject-item"}>
+                        <div className={"subject-item-inner outer-shadow-class"}>
+                            <form>
+
+                                <div className="subject-form-div">
+                                    <Grid container direction="row" direction="row" justifyContent="space-evenly" alignItems="center" >
+                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
+                                            <label htmlFor={'class'} > Class </label>
+                                        </Box>
+                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
+                                            <TextField id="filled-basic" label="Class" variant="filled" style={{ width: 220 }} />
+                                        </Box>
+                                    </Grid>
+                                </div>
+
+                                <div className="subject-form-div">
+                                    <Grid container direction="row" direction="row" justifyContent="space-evenly" alignItems="center" >
+                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
+                                            <label htmlFor={'classType'}> Class Type </label>
+                                        </Box>
+                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
+                                            <Select labelId="demo-simple-select-label" id="demo-simple-select" style={{ width: 220 }}
+                                                    className={'classSize'} onChange={event => this.onChange(event)}>
+                                                {
+                                                    this.state.grade.map(type =>
+                                                        <MenuItem key={type} value={type}>{type}</MenuItem>
+                                                    )
+                                                }
+                                            </Select>
+                                        </Box>
+                                    </Grid>
+                                </div>
+
+                                <div className="subject-form-div">
+                                    <Grid container direction="row" direction="row" justifyContent="space-evenly" alignItems="center" >
+                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
+                                            <label htmlFor={'teacher'}> Teacher  </label>
+                                        </Box>
+                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
+                                            <Select labelId="demo-simple-select-label" id="demo-simple-select" style={{ width: 220 }}
+                                                    className={'classSize'} onChange={event => this.onChange(event)}>
+                                                {
+                                                    this.state.teacher.map(Teacher =>
+                                                        <MenuItem key={Teacher} value={Teacher}>{Teacher}</MenuItem>
+                                                    )
+                                                }
+                                            </Select>
+                                        </Box>
+                                    </Grid>
+                                </div>
+
+                                {/*<Grid container xs={12}>*/}
+                                <div className="subject-form-div">
+                                    <Grid container item direction="row" justifyContent="flex-end" alignItems="baseline" >
+
+                                        <Box ccomponent="div" display="inline" style={{ padding: 10 }} >
+                                            <Button variant="contained" color="secondary">
+                                                Reset
+                                            </Button>
+                                        </Box>
+
+                                        <Box component="div" display="inline" style={{ padding: 10 }} >
+                                            <Button variant="contained" color="primary">
+                                                Add Subject
+                                            </Button>
+                                        </Box>
+
+                                    </Grid>
+                                </div>
+                                {/*</Grid>*/}
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    }
+}
+
+export default CreateSubject;
