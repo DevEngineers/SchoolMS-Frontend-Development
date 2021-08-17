2481455d583039be24386c902952e24c3e4557a5
Mohamed Akeel <akeel1447@gmail.com>
2021-08-17 17:28:10 +0530

    CreateUserAccount route added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import CreateClassTimetable from "./components/classTimetableManagement/CreateClassTimetable";
 import CreateExamTimetable from "./components/examTimetableManagement/CreateExamTimetable";
 import StoreResult from "./components/resultManagement/StoreResult";
+import CreateUserAccount from "./components/userManagement/CreateUserAccount";
 
 function App() {
   return <div>
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/createClassTimetable" component={CreateClassTimetable}/>
         <Route path="/createExamTimetable" component={CreateExamTimetable}/>
         <Route path="/storeResults" component={StoreResult}/>
+        <Route path="/createUsers" component={CreateUserAccount}/>
       </Switch>
     </Router>
   </div>
