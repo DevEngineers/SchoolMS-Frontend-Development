511fbbc52ca17e43f98740006f2c8c2c82244457
Mohamed Akeel <akeel1447@gmail.com>
2021-08-18 14:58:01 +0530

    New Services Created

---
+++ src/services/ExamTimetableService.js
@@ -0,0 +1,96 @@
+const EXAM_TIMETABLE_API_BASE_URI='';
+
+/**
+ * @author : M.N.M Akeel
+ * Registration Number : IT19153414
+ */
+
+class ExamTimetableService{
+    /**
+     *  This service function is to send exam timetable details to backend
+     */
+    async generateExamTimetable(examTimetable){
+        const bearer = 'Bearer ' + localStorage.getItem('userToken');
+        return await fetch(EXAM_TIMETABLE_API_BASE_URI,{
+            method:'POST',
+            headers:{
+                'content-Type':"application/json",
+                'Authorization': bearer
+            },
+            body:JSON.stringify(examTimetable)
+        }).then(response =>{
+            return response;
+        }).catch(reason => {
+            return reason;
+        })
+    }
+
+    /**
+     *  This service function is to Get All Exam timetables from backend
+     */
+    async getexamTimetable(){
+        return await fetch(EXAM_TIMETABLE_API_BASE_URI+"/",{
+            method:'GET',
+        }).then(response =>{
+            return response.json();
+        }).catch(reason => {
+            return reason;
+        })
+
+    }
+
+    /**
+     *  This service function is to get a exam timetable from backend
+     */
+    async getExamTimetableByID(id){
+        return await fetch(EXAM_TIMETABLE_API_BASE_URI+"/"+id,{
+            method:'GET',
+        }).then(response =>{
+            return response.json();
+        }).catch(reason => {
+            return reason;
+        })
+
+    }
+
+
+    /**
+     *  This service function is to update stored exam timetable in backend
+     */
+    async updateExamTimetable(id,examTimetable){
+        const bearer = 'Bearer ' + localStorage.getItem('userToken');
+        return await fetch(EXAM_TIMETABLE_API_BASE_URI+"/"+id,{
+            method:'PUT',
+            headers:{
+                'content-Type':"application/json",
+                'Authorization': bearer
+            },
+            body:JSON.stringify(examTimetable)
+        }).then(response =>{
+            return response;
+        }).catch(reason => {
+            return reason;
+        })
+    }
+
+    /**
+     *  This service function is to Remove stored exam timetable in backend
+     */
+    async removeExamTimetable(id){
+        const bearer = 'Bearer ' + localStorage.getItem('userToken');
+        return await fetch(EXAM_TIMETABLE_API_BASE_URI+"/"+id,{
+            headers:{
+                'Authorization': bearer
+            },
+            method:'DELETE',
+        }).then(response =>{
+            return response;
+        }).catch(reason => {
+            return reason;
+        })
+
+    }
+
+}
+
+export default new ExamTimetableService();
