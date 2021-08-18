4a4793169f7233b5008c426a2df9ff230caf082d
nushkymohamed <nuskymohamed1999@gmai.com>
2021-08-18 15:25:29 +0530

    Create + List UI Created

---
+++ src/Services/StudentPaymentService.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const STUDENTPAYMENT_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+
+class StudentPaymentService {
+
+    getPayments(){
+        return axios.get(STUDENTPAYMENT_API_BASE_URL);
+    }
+
+    createPayment(payment){
+        return axios.post(STUDENTPAYMENT_API_BASE_URL, payment);
+    }
+
+    getPaymentById(paymentId){
+        return axios.get(STUDENTPAYMENT_API_BASE_URL + '/' + paymentId);
+    }
+
+    updatePayment(payment, paymentId){
+        return axios.put(STUDENTPAYMENT_API_BASE_URL + '/' + paymentId, payment);
+    }
+
+    deletePayment(paymentId){
+        return axios.delete(STUDENTPAYMENT_API_BASE_URL + '/' + paymentId);
+    }
+}
+
+export default new StudentPaymentService()
