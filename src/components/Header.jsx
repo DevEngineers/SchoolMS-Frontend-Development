861557a3b2fafe75c4e0d6366b51a7dfd380f6e9
Zumry <amzumry@gmail.com>
2021-08-19 13:49:46 +0530

    Header UI Design implementing 1.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Card, Paper} from "@material-ui/core";
+import '../styles/HeaderFooter.css';
 
 /**
  * @author : A.M Zumry
@@ -24,7 +26,63 @@ class Header extends Component {
 
     render() {
         return <div>
-            <h1>Hello Words</h1>
+            <div  id={'HeadDiv'}>
+                <label id={'HeadTitle'}>Gateway International School</label>
+                <div id={'logRDiv'}>
+                    <a href={'#'} id={'regLink'}>User Profile</a>
+                    <a href={'#'} id={'logLink'} onClick={event => this.logout(event)} >Logout</a>
+                </div>
+
+            </div>
+            <div>
+                <div>
+                    <nav id={'bigDiv'}>
+
+                        <label htmlFor={'drop'} className={'toggle'}>Menu</label>
+                        <input type={'checkbox'} id={'drop'}/>
+                        <ul className={'menu'}>
+                            <li><a href="/">Home</a></li>
+                            <li>
+                                {/*First Tier Drop Down*/}
+                                <label htmlFor={'drop-1'} className={'toggle'}>Authors +</label>
+                                <a href="#">Authors</a>
+                                <input type={'checkbox'} id={'drop-1'}/>
+                                <ul>
+                                    <li><a href="/callForPaper">Call For Papers</a></li>
+                                    <li><a href="/paperSubmission">Submission Instructions</a></li>
+                                    <li><a href="/importantDates">ImportantDates</a></li>
+                                </ul>
+
+                            </li>
+                            <li>
+
+                                {/* First Tier Drop Down */}
+                                <label htmlFor={'drop-2'} className={'toggle'}>Workshop +</label>
+                                <a href="#">Workshop</a>
+                                <input type={'checkbox'} id={'drop-2'}/>
+                                <ul>
+                                    <li><a href="/workShopAllView">Workshops</a></li>
+                                    <li><a href="/WorkshopSubmission">Submission Instructions</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                {/*First Tier Drop Down*/}
+                                <label htmlFor={'drop-3'} className={'toggle'}>Attendees +</label>
+                                <a href="#">Attendees</a>
+                                <input type={'checkbox'} id={'drop-3'}/>
+
+                                <ul>
+                                    <li><a href="/attendeesRegistration">Registrations</a></li>
+                                    <li><a href="/attendeesPayment">By Ticket</a></li>
+                                    <li><a href="/attendeesTickets">View Tickets</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="/templatesDownload">Templates</a></li>
+                            <li><a href="/about">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
     }
 }
