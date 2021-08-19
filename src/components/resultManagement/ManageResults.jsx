a7487fc0b8fea643ae3134a20374337cae0cbb5b
Mohamed Akeel <akeel1447@gmail.com>
2021-08-19 23:46:28 +0530

    ManageResults.jsx 40% implementation complete

--- src/components/resultManagement/ManageResults.jsx
+++ src/components/resultManagement/ManageResults.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
+import {IconButton, TextField} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import PageViewIcon from '@material-ui/icons/Pageview';
+import EditIcon from '@material-ui/icons/Edit';
 
 /**
  * @author : M.N.M Akeel
@@ -6,21 +10,37 @@ import React from "react";
  */
 
 
-class ManageResults extends React.Component{
-    constructor(props) {
-        super(props);
+function ManageResults(props){
+    const [classTimetable,setClassTimetable] = useState([])
 
-        this.state ={
-
-        }
-    }
-
-    render() {
-        return <div>
+    return <div>
+        <div id={'searchDiv'}>
+            <TextField type={'text'}/>
+            <input type={'submit'} value={'Search'}/>
+        </div>
+        <div id={'viewDiv'}>
+            <div>
+                    <label>Grade 11 Class Timetable</label>
+                    <label>Year : 2021</label>
+            </div>
+            <div>
+                <IconButton aria-label="pageView" style={{backgroundColor:"transparent"}}>
+                    <PageViewIcon/>
+                </IconButton>
+            </div>
+            <div>
+                <IconButton aria-label="edit" style={{backgroundColor:"transparent"}}>
+                    <EditIcon/>
+                </IconButton>
+            </div>
+            <div>
+                <IconButton aria-label="delete" style={{backgroundColor:"transparent"}}>
+                    <DeleteIcon/>
+                </IconButton>
+            </div>
+        </div>
 
         </div>
-    }
-
 }
 
 export default ManageResults;
