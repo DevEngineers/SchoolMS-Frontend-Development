5cee869d65cb2bea6e1f5249b5bdcc76bc6b586b
nushkymohamed <nuskymohamed1999@gmai.com>
2021-08-20 22:31:16 +0530

    Student Payment Service Implementation Modified

--- src/Services/StudentPaymentService.js
+++ src/Services/StudentPaymentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const STUDENTPAYMENT_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const STUDENTPAYMENT_API_BASE_URL = "http://localhost:8087/payments/";
 
 class StudentPaymentService {
 
