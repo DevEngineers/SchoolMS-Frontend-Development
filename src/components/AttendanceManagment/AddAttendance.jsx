2817f31fc5cdf98e305ebda77e6664ae1ee23144
Zumry <amzumry@gmail.com>
2021-08-20 22:34:25 +0530

    Store Attendance UI Design implemented 1.0

--- src/components/AttendanceManagment/AddAttendance.jsx
+++ src/components/AttendanceManagment/AddAttendance.jsx
@@ -15,7 +15,7 @@ class AddAttendance extends Component {
         super(props);
         this.state= {
             classType:['A','B','C'],
-            teacher:['Nimal', 'Kumar', 'Kasun']
+            class:['8', '9', '10', '11', '12']
         }
     }
 
@@ -39,77 +39,128 @@ class AddAttendance extends Component {
 
                 <div className={"attendance-row"}>
                     <div className={"attendance-item"}>
-                        <div className={"attendance-item-inner outer-shadow-attendance"}>
-                            <form>
+                        <div className={"attendance-item-big-inner outer-shadow-attendance"}>
+                                <form>
+                                    <div className="attendance-form-div">
+                                        <Grid container direction="row" direction="row" justifyContent="flex-start" alignItems="center" >
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
+                                                <label htmlFor={'date'} > Date </label>
+                                            </Box>
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
+                                                <TextField id="date"  type="date" style={{ width: 220 }} />
+                                            </Box>
+                                        </Grid>
+                                    </div>
 
-                                <div className="attendance-form-div">
-                                    <Grid container direction="row" direction="row" justifyContent="space-evenly" alignItems="center" >
-                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
-                                            <label htmlFor={'class'} > Class </label>
-                                        </Box>
-                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
-                                            <TextField id="filled-basic" label="Class" variant="filled" style={{ width: 220 }} />
-                                        </Box>
-                                    </Grid>
-                                </div>
+                                    <div className="attendance-form-div">
+                                        <Grid container direction="row" direction="row" justifyContent="flex-start" alignItems="center" >
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
+                                                <label htmlFor={'class'} > Class </label>
+                                            </Box>
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
+                                                <Select labelId="demo-simple-select-label" id="demo-simple-select" style={{ width: 220 }}
+                                                        className={'classSize'} onChange={event => this.onChange(event)}>
+                                                    {
+                                                        this.state.class.map(cls =>
+                                                            <MenuItem key={cls} value={cls}>{cls}</MenuItem>
+                                                        )
+                                                    }
+                                                </Select>
+                                            </Box>
+                                        </Grid>
+                                    </div>
 
-                                <div className="attendance-form-div">
-                                    <Grid container direction="row" direction="row" justifyContent="space-evenly" alignItems="center" >
-                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
-                                            <label htmlFor={'classType'}> Class Type </label>
-                                        </Box>
-                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
-                                            <Select labelId="demo-simple-select-label" id="demo-simple-select" style={{ width: 220 }}
-                                                    className={'classSize'} onChange={event => this.onChange(event)}>
-                                                {
-                                                    this.state.classType.map(type =>
-                                                        <MenuItem key={type} value={type}>{type}</MenuItem>
-                                                    )
-                                                }
-                                            </Select>
-                                        </Box>
-                                    </Grid>
-                                </div>
+                                    <div className="attendance-form-div" id="attendance-big-item-end">
+                                        <Grid container direction="row" direction="row" justifyContent="flex-start" alignItems="center" >
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
+                                                <label htmlFor={'classType'}> Class Type </label>
+                                            </Box>
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
+                                                <Select labelId="demo-simple-select-label" id="demo-simple-select" style={{ width: 220 }}
+                                                        className={'classSize'} onChange={event => this.onChange(event)}>
+                                                    {
+                                                        this.state.classType.map(type =>
+                                                            <MenuItem key={type} value={type}>{type}</MenuItem>
+                                                        )
+                                                    }
+                                                </Select>
+                                            </Box>
+                                        </Grid>
+                                    </div>
 
-                                <div className="attendance-form-div">
-                                    <Grid container direction="row" direction="row" justifyContent="space-evenly" alignItems="center" >
-                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
-                                            <label htmlFor={'teacher'}> Teacher  </label>
-                                        </Box>
-                                        <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
-                                            <Select labelId="demo-simple-select-label" id="demo-simple-select" style={{ width: 220 }}
-                                                    className={'classSize'} onChange={event => this.onChange(event)}>
-                                                {
-                                                    this.state.teacher.map(Teacher =>
-                                                        <MenuItem key={Teacher} value={Teacher}>{Teacher}</MenuItem>
-                                                    )
-                                                }
-                                            </Select>
-                                        </Box>
-                                    </Grid>
-                                </div>
+                                </form>
 
-                                {/*<Grid container xs={12}>*/}
-                                <div className="attendance-form-div">
-                                    <Grid container item direction="row" justifyContent="flex-end" alignItems="baseline" >
+                            <div className={"attendance-item-inner outer-shadow-attendance"}>
+                                <form>
 
-                                        <Box ccomponent="div" display="inline" style={{ padding: 10 }} >
-                                            <Button variant="contained" color="secondary">
-                                                Cancel
-                                            </Button>
-                                        </Box>
+                                    <div className="attendance-form-div">
+                                        <Grid container direction="row" direction="row" justifyContent="space-evenly" alignItems="center" >
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
+                                                <label htmlFor={'class'} > Class </label>
+                                            </Box>
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
+                                                <TextField id="filled-basic" label="Class" variant="filled" style={{ width: 220 }} />
+                                            </Box>
+                                        </Grid>
+                                    </div>
 
-                                        <Box component="div" display="inline" style={{ padding: 10 }} >
-                                            <Button variant="contained" color="primary">
-                                                Add Class
-                                            </Button>
-                                        </Box>
+                                    <div className="attendance-form-div">
+                                        <Grid container direction="row" direction="row" justifyContent="space-evenly" alignItems="center" >
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
+                                                <label htmlFor={'classType'}> Class Type </label>
+                                            </Box>
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
+                                                <Select labelId="demo-simple-select-label" id="demo-simple-select" style={{ width: 220 }}
+                                                        className={'classSize'} onChange={event => this.onChange(event)}>
+                                                    {
+                                                        this.state.classType.map(type =>
+                                                            <MenuItem key={type} value={type}>{type}</MenuItem>
+                                                        )
+                                                    }
+                                                </Select>
+                                            </Box>
+                                        </Grid>
+                                    </div>
 
-                                    </Grid>
-                                </div>
-                                {/*</Grid>*/}
+                                    <div className="attendance-form-div">
+                                        <Grid container direction="row" direction="row" justifyContent="space-evenly" alignItems="center" >
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 100 }} >
+                                                <label htmlFor={'teacher'}> Teacher  </label>
+                                            </Box>
+                                            <Box ccomponent="div" display="inline" style={{ padding: 2, width: 250 }} >
+                                                <Select labelId="demo-simple-select-label" id="demo-simple-select" style={{ width: 220 }}
+                                                        className={'classSize'} onChange={event => this.onChange(event)}>
+                                                    {
+                                                        this.state.class.map(Teacher =>
+                                                            <MenuItem key={Teacher} value={Teacher}>{Teacher}</MenuItem>
+                                                        )
+                                                    }
+                                                </Select>
+                                            </Box>
+                                        </Grid>
+                                    </div>
 
-                            </form>
+                                </form>
+                            </div>
+
+                            <div className="attendance-form-div">
+                                <Grid container item direction="row" justifyContent="flex-end" alignItems="baseline" >
+
+                                    <Box ccomponent="div" display="inline" style={{ padding: 10 }} >
+                                        <Button variant="contained" color="secondary">
+                                            Cancel
+                                        </Button>
+                                    </Box>
+
+                                    <Box component="div" display="inline" style={{ padding: 10 }} >
+                                        <Button variant="contained" color="primary">
+                                            Add Class
+                                        </Button>
+                                    </Box>
+
+                                </Grid>
+                            </div>
+
                         </div>
                     </div>
                 </div>
