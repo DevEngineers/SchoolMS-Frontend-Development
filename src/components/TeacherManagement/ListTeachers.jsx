f5a851747e57f7b72880773dbe9248deea71e06c
nushkymohamed <nuskymohamed1999@gmai.com>
2021-08-21 21:01:24 +0530

    List Teacher UI Design Modified

--- src/components/TeacherManagement/ListTeachers.jsx
+++ src/components/TeacherManagement/ListTeachers.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import TeacherService from "../../Services/TeacherService";
 import {IconButton} from "@material-ui/core";
-import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 import PageViewIcon from "@material-ui/icons/Pageview";
 
 class ListTeachers extends Component {
