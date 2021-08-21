2324946a9156eef2d56b1aecceac47a475cb6299
nushkymohamed <nuskymohamed1999@gmai.com>
2021-08-21 18:05:55 +0530

    All Routes Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
-import CreateStudent from "./components/StudentManagement/CreateStudent";
-import ListStudents from "./components/StudentManagement/ListStudents";
-import CreateTeacher from "./components/TeacherManagement/CreateTeacher";
-import CreatePayment from "./components/StudentPaymentManagement/CreatePayment";
-//import ListPayments from "./components/StudentPaymentManagement/ListPayments";
-//import ListTeachers from "./components/TeacherManagement/ListTeachers";
 
+import ListStudents from "./components/StudentManagement/ListStudents";
+import CreateStudent from "./components/StudentManagement/CreateStudent";
+import UpdateStudent from "./components/StudentManagement/UpdateStudent";
+import CreatePayment from "./components/StudentPaymentManagement/CreatePayment";
+import ListPayments from "./components/StudentPaymentManagement/ListPayments";
+import UpdatePayment from "./components/StudentPaymentManagement/UpdatePayment";
+import ListTeachers from "./components/TeacherManagement/ListTeachers";
+import CreateTeacher from "./components/TeacherManagement/CreateTeacher";
+import UpdateTeacher from "./components/TeacherManagement/UpdateTeacher";
+import ViewPayments from "./components/StudentPaymentManagement/ViewPayments";
 
 function App() {
   return <div>
     <Router>
       <Switch>
-        <Route path = "/" exact component = {ListStudents}></Route>
-        <Route path = "/students" component = {ListStudents}></Route>
-        <Route path = "/add-student/:id" component = {CreateStudent}></Route>
-        <Route path = "/add-teacher/:id" component = {CreateTeacher}></Route>
+        <Route path = "/" exact component = {ListPayments}></Route>
+        <Route path = "/payments" component = {ListPayments}></Route>
         <Route path = "/add-payment/:id" component = {CreatePayment}></Route>
-
+        <Route path = "/update-payment/:id" component = {UpdatePayment}></Route>
+        <Route path = "/add-student/:id" component = {CreateStudent}></Route>
+        <Route path = "/update-student/:id" component = {UpdateStudent}></Route>
+        <Route path = "/students" component = {ListStudents}></Route>
+        <Route path = "/add-teacher/:id" component = {CreateTeacher}></Route>
+        <Route path = "/update-teacher/:id" component = {UpdateTeacher}></Route>
+        <Route path = "/teachers" component = {ListTeachers}></Route>
+        <Route path = "/view-payment/:id" component = {ViewPayments}></Route>
 
       </Switch>
     </Router>
