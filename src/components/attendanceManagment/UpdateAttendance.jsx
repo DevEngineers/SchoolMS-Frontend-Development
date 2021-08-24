82085ae5a8388c9c8ec20c96cba2c1e7e287ad2f
Zumry <amzumry@gmail.com>
2021-08-25 01:07:59 +0530

    Update CRUD implementing

--- src/components/attendanceManagment/UpdateAttendance.jsx
+++ src/components/attendanceManagment/UpdateAttendance.jsx
@@ -30,6 +30,49 @@ const options = {
 }
 
 class UpdateAttendance extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            rDate:'',
+            rClass:'',
+            rClassType:'',
+
+            allAttendance:[],
+            studentID:['0001', '0002', '0003','0004', '0005','0006' ],
+            student:['Nimal Kumar', 'Pasan Bandara', 'Kasun kumar','Sunil sunil', 'Tharuni bandara','Kasun Vimal' ],
+            attendance:[1, 1, 1, 1, 0, 1],
+
+            classType:['A','B','C','D','E'],
+            class:['8', '9', '10', '11', '12']
+        }
+    }
+
+    onCheckBox(event){
+        const{value} = event.target;
+        console.log(value);
+
+        // if(student.includes(value) === true){
+        //     let index = student.indexOf(value);
+        //     student.splice(index,1)
+        //     console.log('true students',student);
+        //     return
+        // }
+        //
+        // if(student.includes(value) === false){
+        //     student.push(value);
+        //     console.log('false students',student);
+        // }
+
+    }
+
+    /**
+     * this function is to capture data in the input fields
+     */
+    onChange(event){
+        const { name, value } = event.target;
+        this.setState({ [name] : value });
+    }
+
     render() {
         return (
             <div>
