2f43b1f9bc8fa4f9e89b4c4cbf607baa3ea20e85
Zumry <amzumry@gmail.com>
2021-08-25 01:01:04 +0530

    implementing

---
+++ src/components/attendanceManagment/UpdateAttendance.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+/**
+ * @author : A.M Zumry
+ * Registration Number : IT19175126
+ */
+
+class UpdateAttendance extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default UpdateAttendance;
