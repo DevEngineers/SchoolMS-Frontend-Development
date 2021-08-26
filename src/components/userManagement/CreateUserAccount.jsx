ef1939c4b17e66de8857ee860ca28f68cedc118b
Mohamed Akeel <akeel1447@gmail.com>
2021-08-26 23:17:26 +0530

    Designing complete

--- src/components/userManagement/CreateUserAccount.jsx
+++ src/components/userManagement/CreateUserAccount.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import Select from "@material-ui/core/Select";
 import {Container, MenuItem, TextField} from "@material-ui/core";
 import '../../styles/usersStyles/Users.css'
+import {toast, ToastContainer} from "material-react-toastify";
+import 'material-react-toastify/dist/ReactToastify.css';
+import UserService from "../../services/UserService";
 
 /**
  * @author : M.N.M Akeel
  * Registration Number : IT19153414
  */
 
+//Toast Message Configuration
+const options = {
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: false
+}
 
 class CreateUserAccount extends React.Component{
     constructor(props) {
@@ -34,15 +46,45 @@ class CreateUserAccount extends React.Component{
     }
 
 
-    createUserAccount(event) {
-        event.preventDefault();
-        console.log(this.state.email)
-        console.log(this.state.checkedTimeSlot)
+    createUserAccount() {
+        let user = {
+            branch:this.state.smBranch,
+            username:this.state.username,
+            email:this.state.email,
+            userType:this.state.uType,
+            password:this.state.password
+        }
+
+        if(user.branch === ''){
+            toast.warn('Select the Branch',options)
+        }else if(user.username === ''){
+            toast.warn('Enter the username',options)
+        }else if(user.email === '') {
+            toast.warn('Enter user email', options)
+        }else if(user.userType === '') {
+            toast.warn('Select the user type', options)
+        }else if(user.password === '') {
+            toast.warn('Enter user password', options)
+        }else{
+            UserService.storeUser(user)
+                .then(res => {
+                    if (res.status === 200) {
+                        toast.success("User Account Created Successfully", options)
+                        setTimeout(()=>{this.props.history.push("/manageUsers")},3000)
+                    } else {
+                        throw Error('Something went wrong!! Try again.');
+                    }
+                })
+                .catch((error) => {
+                    toast.error(error.message, options)
+                })
+        }
     }
 
 
     render() {
         return <div>
+            <ToastContainer/>
             <div>
                 <div className={'box'}>
                     <label className={'custom-underline'}>CREATE USER ACCOUNTS</label>
@@ -62,7 +104,7 @@ class CreateUserAccount extends React.Component{
                         <div id={'userSelectOpt'}>
                             <Select labelId="demo-simple-select-label" id="demo-simple-select" name={'smBranch'} value={this.state.smBranch}
                                     className={'userSize'} onChange={event => this.onChange(event)} displayEmpty>
-                                <MenuItem value={''}><span className={'selectUName'}>Select Class</span></MenuItem>
+                                <MenuItem value={''}><span className={'selectUName'}>Select Branch</span></MenuItem>
                                 {
                                     this.state.sBranch.map(branch =>
                                         <MenuItem key={branch} value={branch} className={'selectUName'}>{branch}</MenuItem>
@@ -93,7 +135,7 @@ class CreateUserAccount extends React.Component{
                         </div>
                     </Container>
                     <div className={'btnEDiv'}>
-                        <input type={'submit'} id={'submitUBtn'} value={'Store Results'}/>
+                        <input type={'submit'} id={'submitUBtn'} value={'Store Results'} onClick={this.createUserAccount.bind(this)}/>
                         <input type={'reset'} id={'restUBtn'} value={'Reset'}/>
                     </div>
 
