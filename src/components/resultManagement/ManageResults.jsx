24c5193ee1deb14d0bddda2f483aecb92ff0d550
Mohamed Akeel <akeel1447@gmail.com>
2021-08-26 21:46:23 +0530

    delete implementation complete

--- src/components/resultManagement/ManageResults.jsx
+++ src/components/resultManagement/ManageResults.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from "react";
-import {TextField} from "@material-ui/core";
+import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField} from "@material-ui/core";
 import '../../styles/timetableAndResultStyles/CommonManage.css';
 import ResultListHolder from "./ResultListHolder";
 import ResultService from "../../services/ResultService";
 import {useHistory} from "react-router-dom";
 import {toast, ToastContainer} from "material-react-toastify";
 import 'material-react-toastify/dist/ReactToastify.css';
+import Button from "@material-ui/core/Button";
 
 /**
  * @author : M.N.M Akeel
@@ -24,7 +25,26 @@ const options = {
 
 function ManageResults(props){
     const history = useHistory();
-    const [results,setResults] = useState([])
+    const [results,setResults] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [deleteResultObj, setDeleteResultObj] = useState('');
+
+    /**
+     * handler to open the alter dialog box and setting up the
+     * relevant result that we going to remove in deleteResultObj state variable
+     */
+    const handleClickOpen = (result) => {
+        setOpen(true);
+        setDeleteResultObj(result);
+    };
+
+    /**
+     * handler to close the alter dialog box
+     */
+    const handleClose = () => {
+        setOpen(false);
+    };
+
 
     useEffect(() =>{
         fetchResults().then();
@@ -49,14 +69,16 @@ function ManageResults(props){
         history.push(`/viewResult/${id}`);
     }
 
-    function deleteResult(result){
-        let id = result._id;
+    function deleteResult(){
+        let id = deleteResultObj._id;
         ResultService.removeResult(id)
             .then(res =>{
                 if(res.status === 200){
+                    handleClose();
                     toast.error("Student Result is Removed",options)
                     setTimeout(()=>{this.props.history.push("/manageResults")},3000)
                 }else{
+                    handleClose();
                     toast.warning("Something went wrong!!,Try again.",options)
                 }
             })
@@ -81,11 +103,27 @@ function ManageResults(props){
             </div>*/}
             {
                 results.map(result =>{
-                    return <ResultListHolder key={result._id} Result={result} deleteResult={deleteResult}
+                    return <ResultListHolder key={result._id} Result={result} handleOpenDeleteAlert={handleClickOpen}
                                              viewResult={viewResult} editResult={updateResult}/>
                 })
             }
         </div>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Alert</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Are you sure to remove this record
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary" style={{fontWeight:'bold'}}>
+                    Cancel
+                </Button>
+                <Button onClick={deleteResult} color="secondary" style={{fontWeight:'bold'}}>
+                    Proceed
+                </Button>
+            </DialogActions>
+        </Dialog>
     </div>
 }
 
