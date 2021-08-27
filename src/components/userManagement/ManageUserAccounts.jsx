e1caa102aec45767a9007449f6a169e07c01b5df
Mohamed Akeel <akeel1447@gmail.com>
2021-08-28 00:29:39 +0530

    Designing is complete

--- src/components/userManagement/ManageUserAccounts.jsx
+++ src/components/userManagement/ManageUserAccounts.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Button from "@material-ui/core/Button";
 import '../../styles/usersStyles/Users.css';
+import ResultService from "../../services/ResultService";
+import UserService from "../../services/UserService";
 
 
 /**
@@ -10,66 +12,63 @@ import '../../styles/usersStyles/Users.css';
 
 
 function ManageUserAccounts(props){
+    const [users,setUsers] = useState({});
 
-        return <div>
-            <div>
-                <div className={'box'}>
-                    <label className={'custom-underline'}>USER ACCOUNTS</label>
-                </div>
-            </div>
-            <div id={'tblDiv'}>
-                <table id={'styled-table'}>
-                    <thead>
-                    <tr>
-                        <td className={'thUData'}>Username</td>
-                        <td className={'thUData'}>Email</td>
-                        <td className={'thUData'}>School Branch</td>
-                        <td className={'thUData'}>Account Type</td>
-                        <td className={'thUData'}>Status</td>
-                        <td className={'thUDataBtn'}>Action</td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td className={'tdUData'}>Kamal K</td>
-                        <td className={'tdUData'}>Kamal@gmail.com</td>
-                        <td className={'tdUData'}>Kandy</td>
-                        <td className={'tdUData'}>Administration staff</td>
-                        <td className={'tdUData'}>Active</td>
-                        <td className={'tdUDataBtn'}>
-                            <Button variant="contained" color="primary">Reset Password</Button>&nbsp;&nbsp;
-                            <Button variant="contained" color="primary">Deactivate</Button>&nbsp;&nbsp;
-                            <Button variant="contained" color="secondary">Remove</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className={'tdUData'}>Saman </td>
-                        <td className={'tdUData'}>Saman@gmail.com</td>
-                        <td className={'tdUData'}>Colombo</td>
-                        <td className={'tdUData'}>Examination staff</td>
-                        <td className={'tdUData'}>Deactivate</td>
-                        <td className={'tdUDataBtn'}>
-                            <Button variant="contained" color="primary">Reset Password</Button>&nbsp;&nbsp;
-                            <Button variant="contained" color="primary">Deactivate</Button>&nbsp;&nbsp;
-                            <Button variant="contained" color="secondary">Remove</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className={'tdUData'}>Nimal N</td>
-                        <td className={'tdUData'}>Nimal@gmail.com</td>
-                        <td className={'tdUData'}>Kandy</td>
-                        <td className={'tdUData'}>Examination staff</td>
-                        <td className={'tdUData'}>Active</td>
-                        <td className={'tdUDataBtn'}>
-                            <Button variant="contained" color="primary">Reset Password</Button>&nbsp;&nbsp;
-                            <Button variant="contained" color="primary">Deactivate</Button>&nbsp;&nbsp;
-                            <Button variant="contained" color="secondary">Remove</Button>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
+    useEffect(() =>{
+        fetchUsers().then();
+    },[]);
+
+    async function fetchUsers(){
+        await UserService.getUsers()
+            .then(users =>{
+                console.log('inside',users)
+                setUsers(users);
+            }).catch(err =>{
+                console.error(err)
+            })
+    }
+
+    console.log(users)
+
+    return <div>
+        <div>
+            <div className={'box'}>
+                <label className={'custom-underline'}>USER ACCOUNTS</label>
             </div>
         </div>
+        <div id={'tblDiv'}>
+            <table id={'styled-table'}>
+                <thead>
+                <tr>
+                    <td className={'thUData'}>Username</td>
+                    <td className={'thUData'}>Email</td>
+                    <td className={'thUData'}>School Branch</td>
+                    <td className={'thUData'}>Account Type</td>
+                    <td className={'thUData'}>Status</td>
+                    <td className={'thUDataBtn'}>Action</td>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    users.map(user =>
+                        <tr key={user._id}>
+                            <td className={'tdUData'}>{user.username}</td>
+                            <td className={'tdUData'}>{user.email}</td>
+                            <td className={'tdUData'}>{user.branch}</td>
+                            <td className={'tdUData'}>{user.userType}</td>
+                            <td className={'tdUData'}>{user.status}</td>
+                            <td className={'tdUDataBtn'}>
+                                <Button variant="contained" color="primary">Reset Password</Button>&nbsp;&nbsp;
+                                <Button variant="contained" color="primary">Deactivate</Button>&nbsp;&nbsp;
+                                <Button variant="contained" color="secondary">Remove</Button>
+                            </td>
+                        </tr>
+                    )
+                }
+                </tbody>
+            </table>
+        </div>
+    </div>
 }
 
 export default ManageUserAccounts;
