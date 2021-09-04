46c44d5c0b18a5d41ecd31a9c1e5b75f1157b236
Mohamed Akeel <akeel1447@gmail.com>
2021-09-04 15:41:52 +0530

    branch service added

---
+++ src/services/BranchService.js
@@ -0,0 +1,24 @@
+const BRANCH_BASE_URI='http://localhost:5000/branch';
+
+/**
+ * @author : M.N.M Akeel
+ * Registration Number : IT19153414
+ */
+
+class BranchService {
+
+    /**
+     *  This service function is to Get All branches from backend
+     */
+    async getBranches() {
+        return await fetch(BRANCH_BASE_URI, {
+            method: 'GET',
+        }).then(response => {
+            return response.json();
+        }).catch(reason => {
+            return reason;
+        })
+    }
+}
+
+export default new BranchService();
