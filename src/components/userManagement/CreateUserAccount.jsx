f664cadfacd8e8122e7e66deb6b439d208d9e815
Mohamed Akeel <akeel1447@gmail.com>
2021-09-05 17:16:42 +0530

    toast message error fixed

--- src/components/userManagement/CreateUserAccount.jsx
+++ src/components/userManagement/CreateUserAccount.jsx
@@ -5,6 +5,7 @@ import '../../styles/usersStyles/Users.css'
 import {toast, ToastContainer} from "material-react-toastify";
 import 'material-react-toastify/dist/ReactToastify.css';
 import UserService from "../../services/UserService";
+import BranchService from "../../services/BranchService";
 
 /**
  * @author : M.N.M Akeel
@@ -32,9 +33,18 @@ class CreateUserAccount extends React.Component{
             uType:'',
             password:'',
             rePassword:'',
-            sBranch:['Kandy','Colombo'],
+            sBranch:[],
             userType:['Administrative Staff', 'Examination Staff'],
         }
+    }
+
+    componentDidMount() {
+        BranchService.getBranches()
+            .then(res =>{
+                this.setState({sBranch:res})
+            }).catch(err => {
+            console.error(err)
+        })
     }
 
     /**
@@ -46,7 +56,19 @@ class CreateUserAccount extends React.Component{
     }
 
 
-    createUserAccount() {
+    restForm(){
+        this.setState({
+            smBranch:'',
+            username:'',
+            email:'',
+            uType:'',
+            password:'',
+            rePassword:''
+        })
+    }
+
+    createUserAccount(event) {
+        event.preventDefault();
         let user = {
             branch:this.state.smBranch,
             username:this.state.username,
@@ -70,7 +92,7 @@ class CreateUserAccount extends React.Component{
                 .then(res => {
                     if (res.status === 200) {
                         toast.success("User Account Created Successfully", options)
-                        setTimeout(()=>{this.props.history.push("/manageUsers")},3000)
+                        //setTimeout(()=>{this.props.history.push("/manageUsers")},3000)
                     } else {
                         throw Error('Something went wrong!! Try again.');
                     }
@@ -102,24 +124,24 @@ class CreateUserAccount extends React.Component{
                             <label className={'classULabel'}>Renter-Password</label>
                         </div>
                         <div id={'userSelectOpt'}>
-                            <div className={'textFieldAc'}>
+                            <div className={'textFieldUAc'}>
                                 <Select labelId="demo-simple-select-label" id="demo-simple-select" name={'smBranch'} value={this.state.smBranch}
                                         className={'userSize'} onChange={event => this.onChange(event)} displayEmpty>
                                     <MenuItem value={''}><span className={'selectUName'}>Select Branch</span></MenuItem>
                                     {
                                         this.state.sBranch.map(branch =>
-                                            <MenuItem key={branch} value={branch} className={'selectUName'}><span className={'selectUName'}>{branch}</span></MenuItem>
+                                            <MenuItem key={branch._id} value={branch._id} className={'selectUName'}><span className={'selectUName'}>{branch.branchName}</span></MenuItem>
                                         )
                                     }
                                 </Select>
                             </div>
-                            <div className={'textFieldAc'}>
+                            <div className={'textFieldUAc'}>
                                 <TextField type={'text'} className={'userSize'}  name={'username'} value={this.state.username} onChange={event => this.onChange(event)}/>
                             </div>
-                            <div className={'textFieldAc'}>
+                            <div className={'textFieldUAc'}>
                                 <TextField type={'text'} className={'userSize'}  name={'email'} value={this.state.email} onChange={event => this.onChange(event)}/>
                             </div>
-                            <div className={'textFieldAc'}>
+                            <div className={'textFieldUAc'}>
                                 <Select labelId="demo-simple-select-label" id="demo-simple-select" name={'uType'} value={this.state.uType}
                                         className={'userSize'} onChange={event => this.onChange(event)} displayEmpty>
                                     <MenuItem value={''}><span className={'selectUName'}>Select Account Type</span></MenuItem>
@@ -130,17 +152,17 @@ class CreateUserAccount extends React.Component{
                                     }
                                 </Select>
                             </div>
-                            <div className={'textFieldAc'}>
+                            <div className={'textFieldUAc'}>
                                 <TextField type={'password'} className={'userSize'} name={'password'} value={this.state.password}  onChange={event => this.onChange(event)}/>
                             </div>
-                            <div className={'textFieldAc'}>
+                            <div className={'textFieldUAc'}>
                                 <TextField type={'password'} className={'userSize'} name={'rePassword'} value={this.state.rePassword} onChange={event => this.onChange(event)}/>
                             </div>
                         </div>
                     </Container>
                     <div className={'btnEDiv'}>
                         <input type={'submit'} id={'submitUBtn'} value={'Store Results'} onClick={this.createUserAccount.bind(this)}/>
-                        <input type={'reset'} id={'restUBtn'} value={'Reset'}/>
+                        <input type={'reset'} id={'restUBtn'} value={'Reset'} onClick={this.restForm.bind(this)}/>
                     </div>
 
                 </form>
