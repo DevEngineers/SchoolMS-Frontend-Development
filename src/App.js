9bb7071fa9d03d165fd37254e51bccfbd9c7a734
Mohamed Akeel <akeel1447@gmail.com>
2021-09-08 10:51:16 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 import ListStudents from "./components/StudentManagement/ListStudents";
 import CreateStudent from "./components/StudentManagement/CreateStudent";
@@ -42,59 +42,66 @@ import UpdateResult from "./components/resultManagement/UpdateResult";
 import UserProfile from "./components/userManagement/UserProfile";
 import ManageUserAccounts from "./components/userManagement/ManageUserAccounts";
 import ViewClassTimetable from "./components/classTimetableManagement/ViewClassTimetable";
-import ViewExamTimetable from './components/examTimetableManagement/ViewExamTimetable';
+import ViewExamTimetable from "./components/examTimetableManagement/ViewExamTimetable";
 import ViewResult from "./components/resultManagement/ViewResult";
 
-
 function App() {
-  return <Router>
-      <Header/>
+  return (
+    <Router>
+      <Header />
       <Switch>
-          <Route path="/login" component={Login}/>
-          <Route path="/createClassTimetable" component={CreateClassTimetable}/>
-          <Route path="/updateClassTimetable/:id" component={UpdateClassTimetable}/>
-          <Route path="/manageClassTimetable" component={ManageClassTimetable}/>
-          <Route path="/viewClassTimetable/:id" component={ViewClassTimetable}/>
-          <Route path="/createExamTimetable" component={CreateExamTimetable}/>
-          <Route path="/updateExamTimetable/:id" component={UpdateExamTimetable}/>
-          <Route path="/manageExamTimetable" component={ManageExamTimetable}/>
-          <Route path="/viewExamTimetable/:id" component={ViewExamTimetable}/>
-          <Route path="/storeResults" component={StoreResult}/>
-          <Route path="/updateResults/:id" component={UpdateResult}/>
-          <Route path="/manageResults" component={ManageResults}/>
-          <Route path="/viewResult/:id" component={ViewResult}/>
-          <Route path="/createUsers" component={CreateUserAccount}/>
-          <Route path="/userProfile" component={UserProfile}/>
-          <Route path="/manageUserAccount" component={ManageUserAccounts}/>
-          <Route path="/generateResultReport" component={GenerateResultReport}/>
+        <Route path="/login" component={Login} />
+        <Route path="/createClassTimetable" component={CreateClassTimetable} />
+        <Route
+          path="/updateClassTimetable/:id"
+          component={UpdateClassTimetable}
+        />
+        <Route path="/manageClassTimetable" component={ManageClassTimetable} />
+        <Route path="/viewClassTimetable/:id" component={ViewClassTimetable} />
+        <Route path="/createExamTimetable" component={CreateExamTimetable} />
+        <Route
+          path="/updateExamTimetable/:id"
+          component={UpdateExamTimetable}
+        />
+        <Route path="/manageExamTimetable" component={ManageExamTimetable} />
+        <Route path="/viewExamTimetable/:id" component={ViewExamTimetable} />
+        <Route path="/storeResults" component={StoreResult} />
+        <Route path="/updateResults/:id" component={UpdateResult} />
+        <Route path="/manageResults" component={ManageResults} />
+        <Route path="/viewResult/:id" component={ViewResult} />
+        <Route path="/createUsers" component={CreateUserAccount} />
+        <Route path="/userProfile" component={UserProfile} />
+        <Route path="/manageUserAccount" component={ManageUserAccounts} />
+        <Route path="/generateResultReport" component={GenerateResultReport} />
 
-          <Route path="/create-class" component={AddClass}/>
-          <Route path="/update-class/:id" component={UpdateClass}/>
-          <Route path="/view-class" component={ViewClass}/>
-          <Route path="/create-subject" component={AddSubject}/>
-          <Route path="/update-subject/:id" component={UpdateSubject}/>
-          <Route path="/view-subject" component={ViewSubject}/>
-          <Route path="/store-attendance" component={StoreAttendance}/>
-          <Route path="/update-attendance/:id" component={UpdateAttendance}/>
-          <Route path="/view-attendance" component={ViewAttendance}/>
-          <Route path="/report-attendance" component={ReportAttendance}/>
+        <Route path="/create-class" component={AddClass} />
+        <Route path="/update-class/:id" component={UpdateClass} />
+        <Route path="/view-class" component={ViewClass} />
+        <Route path="/create-subject" component={AddSubject} />
+        <Route path="/update-subject/:id" component={UpdateSubject} />
+        <Route path="/view-subject" component={ViewSubject} />
+        <Route path="/store-attendance" component={StoreAttendance} />
+        <Route path="/update-attendance/:id" component={UpdateAttendance} />
+        <Route path="/view-attendance" component={ViewAttendance} />
+        <Route path="/report-attendance" component={ReportAttendance} />
 
-          <Route path = "/students"  component = {ListStudents}/>
-          <Route path = "/add-student/" component = {CreateStudent}/>
-          <Route path = "/update-student/:id" component = {UpdateStudent}/>
-          <Route path = "/view-student/:id" component = {ViewStudents}/>
-          <Route path = "/add-teacher/" component = {CreateTeacher}/>
-          <Route path = "/update-teacher/:id" component = {UpdateTeacher}/>
-          <Route path = "/teachers" component = {ListTeachers}/>
-          <Route path = "/view-teacher/:id" component = {ViewTeachers}/>
-          <Route path = "/add-payment/" component = {CreatePayment}/>
-          <Route path = "/update-payment/:id" component = {UpdatePayment}/>
-          <Route path = "/view-payment/:id" component = {ViewPayments}/>
-          <Route path = "/payments" component = {ListPayments}/>
-          <Route path = "/payment-report" component = {GeneratePaymentReport}/>
+        <Route path="/students" component={ListStudents} />
+        <Route path="/add-student/" component={CreateStudent} />
+        <Route path="/update-student/:id" component={UpdateStudent} />
+        <Route path="/view-student/:id" component={ViewStudents} />
+        <Route path="/add-teacher/" component={CreateTeacher} />
+        <Route path="/update-teacher/:id" component={UpdateTeacher} />
+        <Route path="/teachers" component={ListTeachers} />
+        <Route path="/view-teacher/:id" component={ViewTeachers} />
+        <Route path="/add-payment/" component={CreatePayment} />
+        <Route path="/update-payment/:id" component={UpdatePayment} />
+        <Route path="/view-payment/:id" component={ViewPayments} />
+        <Route path="/payments" component={ListPayments} />
+        <Route path="/payment-report" component={GeneratePaymentReport} />
       </Switch>
-      <Footer/>
+      <Footer />
     </Router>
+  );
 }
 
 export default App;
