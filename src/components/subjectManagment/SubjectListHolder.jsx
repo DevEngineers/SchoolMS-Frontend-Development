73c050c0c270ff46714cc14ed8f661be8686d2a6
Zumry <amzumry@gmail.com>
2021-09-17 17:07:58 +0530

    final commit

--- src/components/subjectManagment/SubjectListHolder.jsx
+++ src/components/subjectManagment/SubjectListHolder.jsx
@@ -2,27 +2,27 @@ import {IconButton} from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import React from "react";
-import '../../styles/subjectManagment/ViewSubject.css';
+import "../../styles/subjectManagment/ViewSubject.css";
 
 export default function SubjectListHolder(props){
     const {subject,editSubject,handleOpenDeleteAlert} = props
 
     return <div className="SubjectListHolder">
-        <div id={'viewMain'}>
+        <div id={"viewMain"}>
 
-            <div id={'viewDiv'}>
-                <div id={'viewSmall'}>
-                    <label id="ClassLabel">Class : {subject.class.class} </label>
-                    <label id={'SubjectLabel'}>Subject : {subject.subject} </label>
-                    <label id={'TeacherLabel'}>Teacher : {subject.teacher.teacherName} </label>
+            <div id={"viewDiv"}>
+                <div id={"viewSmall"}>
+                    <label id="ClassLabel"> {subject.class.class} </label>
+                    <label id={"SubjectLabel"}>Subject : {subject.subject} </label>
+                    <label id={"TeacherLabel"}>Teacher : {subject.teacher.teacherName} </label>
                 </div>
-                <div className={'buttonDiv'}>
+                <div className={"buttonDiv"}>
                     <IconButton aria-label="delete" style={{backgroundColor:"transparent"}}
                                 onClick={() => handleOpenDeleteAlert(subject)}>
                         <DeleteIcon/>
                     </IconButton>
                 </div>
-                <div className={'buttonDiv'}>
+                <div className={"buttonDiv"}>
                     <IconButton aria-label="edit" style={{backgroundColor:"transparent"}}
                                 onClick={() => editSubject(subject)}>
                         <EditIcon/>
