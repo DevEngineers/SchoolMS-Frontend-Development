b29d9fe9c5ebb6d00049c3d7ddb009d742d54daa
Zumry <amzumry@gmail.com>
2021-09-17 13:52:21 +0530

    final commit

--- src/components/classManagment/ClassListHolder.jsx
+++ src/components/classManagment/ClassListHolder.jsx
@@ -2,27 +2,27 @@ import {IconButton} from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import React from "react";
-import '../../styles/classManagment/ViewClass.css';
+import "../../styles/classManagment/ViewClass.css";
 
 export default function ClassListHolder(props){
     const {Class,editClass,handleOpenDeleteAlert} = props
 
     return <div className="ClassListHolder">
-        <div id={'viewMain'}>
+        <div id={"viewMain"}>
 
-            <div id={'viewDiv'}>
-                <div id={'viewSmall'}>
+            <div id={"viewDiv"}>
+                <div id={"viewSmall"}>
                     <label id="ClassLabel"> {Class.class} </label>
-                    <label id={'TypeLabel'}> {Class.classType.name} </label>
-                    <label id={'TeacherLabel'}>Teacher : {Class.teacher.teacherName} </label>
+                    <label id={"TypeLabel"}> {Class.classType.name} </label>
+                    <label id={"TeacherLabel"}>Teacher : {Class.teacher.teacherName} </label>
                 </div>
-                <div className={'buttonDiv'}>
+                <div className={"buttonDiv"}>
                     <IconButton aria-label="delete" style={{backgroundColor:"transparent"}}
                                 onClick={() => handleOpenDeleteAlert(Class)}>
                         <DeleteIcon/>
                     </IconButton>
                 </div>
-                <div className={'buttonDiv'}>
+                <div className={"buttonDiv"}>
                     <IconButton aria-label="edit" style={{backgroundColor:"transparent"}}
                                 onClick={() => editClass(Class)}>
                         <EditIcon/>
