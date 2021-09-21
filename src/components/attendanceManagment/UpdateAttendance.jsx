f65cfe38d68d9dd2c0202dd3409a739a41792aa9
Zumry <amzumry@gmail.com>
2021-09-21 21:55:49 +0530

    clear unwanted comments

--- src/components/attendanceManagment/UpdateAttendance.jsx
+++ src/components/attendanceManagment/UpdateAttendance.jsx
@@ -12,15 +12,6 @@ import StudentService from "../../services/StudentService";
  * Registration Number : IT19175126
  */
 
-// const students =[
-//     { Id: '0001', name: 'Nimal Kumar', class:'10' , classType:'A', Att:true },
-//     { Id: '0002', name: 'Pasan Bandara', class:'10' , classType:'A', Att:true },
-//     { Id: '0003', name: 'Kasun kumar', class:'10' , classType:'A', Att:true },
-//     { Id: '0004', name: 'Sunil sunil', class:'10' , classType:'A', Att:true },
-//     { Id: '0005', name: 'Tharuni bandara', class:'10' , classType:'A', Att:false },
-//     { Id: '0006', name: 'Kasun Vimal', class:'10' , classType:'A', Att:true },
-// ]
-
 //Toast Message Configuration
 const options = {
     position: "top-center",
