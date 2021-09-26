55fc3da10906e0dd070ff824688b8fe51f5f23e0
Zumry <amzumry@gmail.com>
2021-09-27 00:28:27 +0530

    final commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import HomeIcon from '@material-ui/icons/Home';
-import '../styles/HeaderFooter.css';
+import "../styles/HeaderFooter.css";
 
 /**
  * @authors : A.M Zumry & M.N.M Akeel
@@ -24,27 +24,27 @@ class Header extends Component {
 
     forAdministratorView(){
         return <div>
-            <div  id={'HeadDiv'}>
-                <label id={'HeadTitle'}>𝙂𝙖𝙩𝙚𝙬𝙖𝙮 𝙄𝙣𝙩𝙚𝙧𝙣𝙖𝙩𝙞𝙤𝙣𝙖𝙡 𝙎𝙘𝙝𝙤𝙤𝙡</label>
-                <div id={'logRDiv'}>
-                    <a href={'#'} id={'regLink'}>Manage Users</a>
-                    <a href={'/userProfile'} id={'regLink'}>User Profile</a>
-                    <a href={'#'} id={'logLink'} onClick={event => this.logout(event)} >Logout</a>
+            <div  id={"HeadDiv"}>
+                <label id={"HeadTitle"}>𝙂𝙖𝙩𝙚𝙬𝙖𝙮 𝙄𝙣𝙩𝙚𝙧𝙣𝙖𝙩𝙞𝙤𝙣𝙖𝙡 𝙎𝙘𝙝𝙤𝙤𝙡</label>
+                <div id={"logRDiv"}>
+                    <a href={'#'} id={"regLink"}>Manage Users</a>
+                    <a href={'/userProfile'} id={"regLink"}>User Profile</a>
+                    <a href={'#'} id={"logLink"} onClick={event => this.logout(event)} >Logout</a>
                 </div>
             </div>
             <div>
-                <nav id={'bigDiv'}>
-                    <label htmlFor={'drop'} className={'toggle'}>Menu</label>
-                    <input type={'checkbox'} id={'drop'}/>
-                    <ul className={'menu'}>
-                        <li id={'homeID'}>
+                <nav id={"bigDiv"}>
+                    <label htmlFor={"drop"} className={"toggle"}>Menu</label>
+                    <input type={"checkbox"} id={"drop"}/>
+                    <ul className={"menu"}>
+                        <li id={"homeID"}>
                             <a href="#"><HomeIcon id={'homeIcon'}/></a>
                         </li>
                         <li>
                             {/*First Tier Drop Down*/}
-                            <label htmlFor={'drop-1'} className={'toggle'}>Student</label>
+                            <label htmlFor={"drop-1"} className={"toggle"}>Student</label>
                             <a href="#">Student</a>
-                            <input type={'checkbox'} id={'drop-1'}/>
+                            <input type={"checkbox"} id={"drop-1"}/>
                             <ul>
                                 <li><a href="/add-student/">Add Student</a></li>
                                 <li><a href="/students">View Students</a></li>
@@ -52,9 +52,9 @@ class Header extends Component {
                         </li>
                         <li>
                             {/* First Tier Drop Down */}
-                            <label htmlFor={'drop-2'} className={'toggle'}>Teacher</label>
+                            <label htmlFor={"drop-2"} className={"toggle"}>Teacher</label>
                             <a href="#">Teacher</a>
-                            <input type={'checkbox'} id={'drop-2'}/>
+                            <input type={"checkbox"} id={"drop-2"}/>
                             <ul>
                                 <li><a href="/add-teacher/">Add Teacher</a></li>
                                 <li><a href="/teachers">View Teachers</a></li>
@@ -62,9 +62,9 @@ class Header extends Component {
                         </li>
                         <li>
                             {/*First Tier Drop Down*/}
-                            <label htmlFor={'drop-3'} className={'toggle'}>Class</label>
+                            <label htmlFor={"drop-3"} className={"toggle"}>Class</label>
                             <a href="#">Class</a>
-                            <input type={'checkbox'} id={'drop-3'}/>
+                            <input type={"checkbox"} id={"drop-3"}/>
                             <ul>
                                 <li><a href="/create-class">Add Class</a></li>
                                 <li><a href="/view-class">View Class's</a></li>
@@ -72,29 +72,29 @@ class Header extends Component {
                         </li>
                         <li>
                             {/*First Tier Drop Down*/}
-                            <label htmlFor={'drop-4'} className={'toggle'}>Subject</label>
+                            <label htmlFor={"drop-4"} className={"toggle"}>Subject</label>
                             <a href="#">Subject</a>
-                            <input type={'checkbox'} id={'drop-4'}/>
+                            <input type={"checkbox"} id={"drop-4"}/>
                             <ul>
                                 <li><a href="/create-subject">Add Subject</a></li>
                                 <li><a href="/view-subject">View Subject</a></li>
                             </ul>
                         </li>
-                        <li className={'liChange'}>
+                        <li className={"liChange"}>
                             {/*First Tier Drop Down*/}
-                            <label htmlFor={'drop-5'} className={'toggle'}>Class Timetable</label>
+                            <label htmlFor={"drop-5"} className={"toggle"}>Class Timetable</label>
                             <a href="#">Class Timetable</a>
-                            <input type={'checkbox'} id={'drop-5'}/>
+                            <input type={"checkbox"} id={"drop-5"}/>
                             <ul>
                                 <li><a href="/createClassTimetable">Create Class Timetable</a></li>
                                 <li><a href="/manageClassTimetable">View Class Timetable</a></li>
                             </ul>
                         </li>
-                        <li className={'liChange'}>
+                        <li className={"liChange"}>
                             {/*First Tier Drop Down*/}
-                            <label htmlFor={'drop-6'} className={'toggle'}>Exam Timetable</label>
+                            <label htmlFor={"drop-6"} className={"toggle"}>Exam Timetable</label>
                             <a href="#">Exam Timetable</a>
-                            <input type={'checkbox'} id={'drop-6'}/>
+                            <input type={"checkbox"} id={"drop-6"}/>
                             <ul>
                                 <li><a href="/createExamTimetable">Create Exam Timetable</a></li>
                                 <li><a href="/manageExamTimetable">View Exam Timetable</a></li>
@@ -102,9 +102,9 @@ class Header extends Component {
                         </li>
                         <li>
                             {/*First Tier Drop Down*/}
-                            <label htmlFor={'drop-7'} className={'toggle'}>Results</label>
+                            <label htmlFor={"drop-7"} className={"toggle"}>Results</label>
                             <a href="#">Results</a>
-                            <input type={'checkbox'} id={'drop-7'}/>
+                            <input type={"checkbox"} id={"drop-7"}/>
                             <ul>
                                 <li><a href="/storeResults">Add Results</a></li>
                                 <li><a href="/manageResults">View Results</a></li>
@@ -113,9 +113,9 @@ class Header extends Component {
                         </li>
                         <li>
                             {/*First Tier Drop Down*/}
-                            <label htmlFor={'drop-8'} className={'toggle'}>Attendance</label>
+                            <label htmlFor={"drop-8"} className={"toggle"}>Attendance</label>
                             <a href="#">Attendance</a>
-                                <input type={'checkbox'} id={'drop-8'}/>
+                                <input type={"checkbox"} id={"drop-8"}/>
                             <ul>
                                 <li><a href="/store-attendance">Add Attendance</a></li>
                                 <li><a href="/view-attendance">View Attendance</a></li>
@@ -124,9 +124,9 @@ class Header extends Component {
                         </li>
                         <li>
                             {/*First Tier Drop Down*/}
-                            <label htmlFor={'drop-9'} className={'toggle'}>Fees</label>
+                            <label htmlFor={"drop-9"} className={"toggle"}>Fees</label>
                             <a href="#">Fees</a>
-                            <input type={'checkbox'} id={'drop-9'}/>
+                            <input type={"checkbox"} id={"drop-9"}/>
                             <ul>
                                 <li><a href="/add-payment/">Add Fees</a></li>
                                 <li><a href="/payments">View Fees</a></li>
